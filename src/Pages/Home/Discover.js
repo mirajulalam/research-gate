160648d0046f17e9ccaced6e6ef8e51f7d48cf08
mirajulalam <alifmirajulalam@gmail.com>
2022-12-08 19:20:33 -0800

    discover components added

---
+++ src/Pages/Home/Discover.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import discover from '../../assets/discover.svg';
+
+const Discover = () => {
+    return (
+        <>
+        <section className="max-w-full bg-white py-8 lg:px-28">
+<div className="flex flex-col lg:flex-row justify-center items-center container mx-auto my-10">
+  <div className='flex-1  flex justify-center items-center sm:px-4'>
+  <img src={discover} alt="img" className="lg:max-w-md"/>
+  </div>
+  <div className="flex-1 sm:px-4">
+  <h1 className="text-5xl font-bold text-black">Discover research</h1>
+  <p className='text-neutral text-xl py-5'>Access over 135 million publication pages and stay up to <br/> date with what's happening in your field.</p>
+  <div>
+  <input  className='px-10 py-4 border border-neutral rounded' placeholder='Search publication'/>
+  </div>
+  
+  </div>
+</div>
+</section>
+
+</>
+    );
+};
+
+export default Discover;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Discover from './Discover';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Discover/>
         </div>
     );
 };
